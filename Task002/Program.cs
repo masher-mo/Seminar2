00b30e454cec849b51eec70009c0f34d4b858274
bellk <bellkiss-masher@yandex.ru>
2022-10-21 13:46:15 +0300

    second number

---
+++ Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую йифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+int firstDigit = number / 100;
+int secondDigit = number % 10;
+Console.WriteLine($"Число от {number} это {firstDigit*10 + secondDigit} ");
